8376a4a957ec0e91cc2be646d5c857fa647f4cf4
hyperabstrakt <36825522+EmilSchurr@users.noreply.github.com>
2021-12-11 13:31:50 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+    <footer class="pt-3 mt-4 text-muted border-top">
+          &copy; 2019-2021 emiLANo &middot; by hyperabstrakt &middot; <a href="Kekse.html">Cookies</a> &middot; <a href="/agb.html">Nutzungsbedingungen</a> &middot; <a href="/impressum.html">Impressum</a>
+    </footer>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
