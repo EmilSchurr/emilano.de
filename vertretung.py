f46e7d039c01200035d3c32f6523f152907c0906
hyperabstrakt <36825522+EmilSchurr@users.noreply.github.com>
2021-12-11 13:45:40 +0100

    Create vertretung.py
    Requests timetable in the mornig before everyone else does causing the Server to throw database errors...

---
+++ vertretung.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3.7
+import requests
+from bs4 import BeautifulSoup
+from tika import parser
+import pandas as pd
+
+pURL = "https://*****/*/*/auth/login.php"
+kURL = "https://*****/login.php"
+session = requests.Session()
+f = session.get(kURL)
+def getCsrf():
+  soup = BeautifulSoup(f.text, 'html.parser')
+  csrfv = soup.find('input', {'name': 'csrf'})
+  strcsrfv = str(csrfv)
+  csrfm = strcsrfv[40:]
+  csrf = csrfm[:-3]
+  return(csrf)
+getCsrf()
+login_data = {
+  'csrf': getCsrf(),
+	'username': '*****',
+	'password': '*****',
+	'go_to': ''
+	}
+f = session.post(pURL, data=login_data)
+
+def getEssen():
+	essen = session.get("https://*****/aktuelles/get_file/?f=ep_speisenplan.pdf")
+	open('assets/essen.pdf', 'wb').write(essen.content)
+
+def getVertretung():
+	vertretung = session.get("https://*****/service/vertretungsplan")
+	soup = BeautifulSoup(vertretung.text, 'html.parser')
+	vg = soup.find('div', {'class': 'main_center'})
+	vgstr = str(vg)
+	return (vgstr)
+vgstr = getVertretung()
+getVertretung()
+
+if vgstr == "None":
+	getVertretung()
+	vgstr = getVertretung()
+
+else:
+  g = open('/var/www/html/assets/content/v.php','w')
+  g.write(vgstr)
+  g.close()
